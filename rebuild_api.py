2db0e096a5234b56918125cc8f83c44dab39b549
Jacob.rowland <Jacob.rowland@glasswallsolutions.org>
2020-07-15 12:28:26 -0400

    Main

--- rebuild_api.py
+++ rebuild_api.py
@@ -1,9 +1,20 @@
 import os
 import requests
+import tkinter
+from tkinter import filedialog
+from tkinter import *
+import tkinter as tk
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+
+
 
 api_key = input("Please Enter Your API Key- ")
 url = "https://8oiyjy8w63.execute-api.us-west-2.amazonaws.com/Prod/api/rebuild/file"
-file_extenstions = [".xlsm", ".pdf", ".jpg", ".gif", ".png", ".emf", ".tiff", ".bmp", ".doc", ".doc", ".xls", ".xlt", ".ppt", ".pot", ".docx", ".docm", ".dotx", ".dotm", ".xlsx", ".xlam", ".xltx", ".xltm", ".xpptx", ".potx", ".potx", ".potm", ".pptm", ".ppsx", ".ppam", ".ppsm"]
+file_extenstions = [".xlsm", ".pdf", ".jpg", ".gif", ".png", ".emf", ".tiff", ".bmp", ".doc", ".doc", ".xls", ".xlt", ".ppt", ".pot", ".docx", ".docm", ".dotx", ".dotm", ".xlsx", ".xlam", ".xltx", ".xltm", ".pptx", ".potx", ".potx", ".potm", ".pptm", ".ppsx", ".ppam", ".ppsm"]
 
 
 def list_excel_files(input_folder_path):
@@ -16,10 +27,10 @@ def list_excel_files(input_folder_path):
                 print(os.path.join(input_folder_path, file))
                 excel_files.append(file)
     return excel_files
-
     
     
-def rebuild_files(excel_file_paths, input_folder_path, output_folder_path):
+def rebuild_files(excel_file_paths, input_folder_path, output_folder_path): 
+    
     # Loops through the list passed in, rebuilds each file and stores each one in a new folder.
     for file in (excel_file_paths):
             input_file_path = os.path.join(input_folder_path, file)
@@ -43,8 +54,11 @@ def rebuild_files(excel_file_paths, input_folder_path, output_folder_path):
             else:
                 response.raise_for_status()
 
-input_folder_path = input("Enter Your Input Directory ")
-output_folder_path = input('Enter your output Directory ')
+
+print("Select Input Folder")
+input_folder_path = tkinter.filedialog.askdirectory(initialdir = "/",title = "Select input folder")
+print("Select Output Folder")
+output_folder_path = tkinter.filedialog.askdirectory(initialdir = "/",title = "Select output folder")
 excel_file_list = list_excel_files(input_folder_path)
 
 rebuild_files(excel_file_list, input_folder_path, output_folder_path)
---
+++ test.py
@@ -0,0 +1,9 @@
+import os
+import asyncio
+from pyppeteer import launch
+
+ module.exports = async ({ page }) => {
+  await page.goto('https://dev.curlywurly.me');
+
+};
+      
